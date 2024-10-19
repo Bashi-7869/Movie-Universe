1596cef0fa486bfd2c0acc2ebd9c191a8c12441d
Bashi-7869 <121685218+Bashi-7869@users.noreply.github.com>
2024-10-19 18:27:40 +0530

    showing movieinfo on display

--- Movies/app.js
+++ Movies/app.js
@@ -2,43 +2,41 @@ const movie = [
     {
         id:1,
         title: "IT",
-        category: "Horror",
+        genres: "Horror",
         img: "/images/item1.png",
-        rating:" IMDB rating : 7.3/10",
+        rating: `IMDB rating : 7.3/10`,
         desc:`This movie is based on Stephen King's novel and the plot is revolves around a group of      childeren who face off against a malevolent entiythat takes the form of Pennywise the clown. The film incorporates elements of psychological horror, supernatural suspense, and coming of age themes,making it a notable entry in both horror and thiller cinema.`,
         link:`https://tubitv.com/movies/694396/it`
     },
 
     {
-        id:7,
+        id:2,
         title: "The Jungle Book (2016)",
-        category: "Fantasy",
+        genres: "Fantasy",
         img: "/images/item10.jpg",
-        rating:" IMDB rating : 7.3/10",
+        rating:`IMDB rating : 7.3/10`,
         desc:` A young boy name is Mowgli raiseed by wolves in the jungle. When the fearsome tiger Shere Khan Threatens his life, Mowgli embarks on a journey of self-discovery, guided by hid loyal panther friend Bagheera and the fun-loving bear Baloo`,
         link:`https://www.justwatch.com/in/movie/the-jungle-book-2016`
     
     },
 
-    
-
     {
         id:3,
         title: "The Nun",
-        category: "Horror",
-        img: "/images/item3.jpg",
-        rating:" IMDB rating : 7.3/10",
+        genres: "Horror",
+        img: "/images/item3.jpeg",
+        rating:`IMDB rating : 7.3/10`,
         desc:`A priest with a haunted past and a novice on the threshold of her final vows are sent by the Vatican to investigate the death of a young nun in Romania and confront a malevolent force in the form of a demonic nun`,
         link:`https://www.moviefone.com/movie/the-nun/cXcjBZLs2Llgol5mi0FSa1/main/`
   
     },
 
     {
-        id:7,
+        id:4,
         title:"The Hangover",
-        category: "comedy",
+        genres: "comedy",
         img: "/images/item25.jpg",
-        rating:" IMDB rating : 7.3/10",
+        rating:`IMDB rating : 7.3/10`,
         desc:` Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi sit atque cumque natus totam magnam! Assumenda magnam nihil mollitia accusantium velit perferendis labore.`,
         link:`https://flixup.to/watch-movie/watch-the-hangover-full-19008.5298652`
     
@@ -46,11 +44,11 @@ const movie = [
     },
 
     {
-        id:7,
+        id:5,
         title:"Race Grurram",
-        category: "comedy",
+        genres: "comedy",
         img: "/images/item22.jpg",
-        rating:" IMDB rating : 7.3/10",
+        rating: `IMDB rating : 7.3/10`,
         desc:` Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi sit atque cumque natus totam magnam! Assumenda magnam nihil mollitia accusantium velit perferendis labore.`,
         link:`https://www.youtube.com/watch?v=oVsA6B1cweo`
     
@@ -59,191 +57,201 @@ const movie = [
     {
         id:6,
         title: "Masooda",
-        category: "Horror",
+        genres: "Horror",
         img: "/images/item6.jpg",
-        price:" IMDB rating : 7.3/10",
+        rating:`IMDB rating : 7.3/10`,
         desc:`the schoolteacher Neelam, and her teenage daughter, Nazia possessed ba malevolent spirit, neelam seeks help of her neighbor Gopi and local exorcist to save her.`,
         link:`https://www.airtelxstream.in/movies/masooda/AHA_MOVIE_C6D49DC4-C4D5-495B-86C5-AA6EC41CE315`
     },
     {
         id:7,
-        title: "The Chronicles of Narnia: The Lion, the Witch and the Wardrobe (2005)",
-        category: "Fantasy",
+        title: "The Chronicles of Narnia (2005)",
+        genres: "Fantasy",
         img: "/images/item7.jpg",
-        rating:" IMDB rating : 7.3/10",
+        rating:`IMDB rating : 7.3/10`,
         desc:` The story follows four siblings Peter, Susan, Edmund and Lucy who discover a magical wardrobe that transports them to the mysftcal land of Narnia. In Narnia the children join the fight to free narnia from the which's tyrant.`,
         link:`https://www.justwatch.com/us/movie/the-chronicles-of-narnia-the-lion-the-witch-and-the-wardrobe-2005`
     
     },
 
     {
-        id:4,
+        id:8,
         title: "LA LLorona",
-        category: "Horror",
+        genres: "Horror",
         img: "/images/item4.jpg",
-        rating:" IMDB rating : 7.3/10",
+        rating:` IMDB rating : 7.3/10`,
         desc:`A social worker, Anna, who unknowingley becomes entangled in La Llorona's curse after investingating a family in distress. As La Llorona begins to target Anna's own children, she seeks help from a former priest protect her family from  the malevolent spirit. `,
         link:`https://tubitv.com/movies/480219/the-haunting-of-la-llorona`
   
     },
 
     {
-        id:7,
+        id:9,
         title: "Pinocchio (2019)",
-        category: "Fantasy",
+        genres: "Fantasy",
         img: "/images/item9.jpg",
-        rating:" IMDB rating : 7.3/10",
+        rating:`IMDB rating : 7.3/10`,
         desc:`The story follows a wooden puppet created by the poor carpenter Geppetto.that puppet name is Pinocchio who magically comes to life and how pinocchio  embarks on a journey to become a real boy. `,
         link:`https://letterboxd.com/film/pinocchio-2019/`
     
     },
-
     
     {
-        id:7,
+        id:10,
         title:"Ek The Tiger",
-        category: "action",
+        genres: "action",
         img: "/images/item15.jpg",
-        rating:" IMDB rating : 7.3/10",
+        rating: `IMDB rating : 7.3/10`,
         desc:`In this film a RAW agent name is Tiger who assigned to trackdown a suspected scientist they tiger encounters Zoya a Pakistani intelligence agent as their paths intertwine and how complicated their romance by their duties to their countries.`,
         link:`https://www.primevideo.com/detail/Ek-Tha-Tiger/0F71KQIBA1EP6VNKHG7W9AVTOV`
     
     },
     
     {
-        id:7,
+        id:11,
         title: "Life of Pi (2012)",
-        category: "Fantasy",
+        genres: "Fantasy",
         img: "/images/item11.jpg",
-        rating: " IMDB rating : 7.3/10",
+        rating: `IMDB rating : 7.3/10`,
         desc:` a boy name is Pi Patel,who survived from shipwreck in the pacific ocean.He is stranded on a lifeboat with several animals, including a Beangal tiger named Richard Parker. as the drift across ocean, Pi must find ways to coexist with the tiger while struggling to survive.`,
         link:`https://www.hotstar.com/in/movies/life-of-pi/1770000706`
     
     },
+
+    {
+        id:12,
+        title:"Shutter Island",
+        genres: "Myster",
+        img: "/images/item27.jpg",
+        rating: `IMDB rating : 7.3/10`,
+        desc:` The story take place  at a fully operational dinosaur theme park called Jurassic world there a dinosur called indominus which escapes and wreaks havoc on the park. the park manager Claire Dearing along with dinosaur trainer owen Grady must find a way to save the guests including Clair's nephews.`,
+        link:`https://www.justwatch.com/us/movie/jurassic-world`
+    
+    },
     
     {
-        id:7,
+        id:12,
         title:"Jurassic World",
-        category: "Fantasy",
+        genres: "Fantasy",
         img: "/images/item13.jpg",
-        rating: " IMDB rating : 7.3/10",
+        rating: `IMDB rating : 7.3/10`,
         desc:` The story take place  at a fully operational dinosaur theme park called Jurassic world there a dinosur called indominus which escapes and wreaks havoc on the park. the park manager Claire Dearing along with dinosaur trainer owen Grady must find a way to save the guests including Clair's nephews.`,
         link:`https://www.justwatch.com/us/movie/jurassic-world`
     
     },
 
     {
-        id:2,
+        id:13,
         title: "The Conjuring",
-        category: "Horror",
+        genres: "Horror",
         img: "/images/item2.jpg",
-        rating:" IMDB rating : 7.3/10",
+        rating: `IMDB rating : 7.3/10`,
         desc:`The Conjuring is a 2013 supernatural horror film inspired by the true-life story of the Perron family, who claimed theylived among the dead in the 1970s as spirits both friendly and sinister inhabited their Rhode Island farmhouse.`,
         link:`https://www1.123moviesme.online/the-conjuring-2013/`
     },
     {
-        id:7,
+        id:14,
         title:"John Wick",
-        category: "action",
+        genres: "action",
         img: "/images/item14.jpg",
-        rating:" IMDB rating : 7.3/10",
+        rating: `IMDB rating : 7.3/10`,
         desc:` John Wick a retired hitman how he drawn back into his former life of assassination and he unleashes his lethal skills on those responsible, relentless quest for retribution that pulls him into a world of organized crime.`,
         link:`https://www.justwatch.com/us/movie/john-wick`
     
     },
 
     {
-        id:7,
+        id:15,
         title: "Jumanji: The Next Level (2019)",
-        category: "Fantasy",
+        genres: "Fantasy",
         img: "/images/item8.jpg",
-        rating:" IMDB rating : 7.3/10",
+        rating:` IMDB rating : 7.3/10`,
         desc:`A group of friends who are pulled back into the dangerous jumanji video game world to rescue their friend Spencer.`,
         link:`https://www1.123moviesme.online/jumanji-the-next-level-2019/`
     
     },
 
     {
-        id:7,
+        id:16,
         title:"War",
-        category: "action",
+        genres: "action",
         img: "/images/item16.jpg",
-        rating:" IMDB rating : 7.3/10",
+        rating: `IMDB rating : 7.3/10`,
         desc:` The story revolves around Kabir a elite soldier and mentor and his protege. khalid who is tasked with tracking him down after kabir is declared a traitor. how khalid embarks on a mission to uncover kabir's motives.`,
         link:`https://www1.123moviesme.online/war-2019/`
     
     },
 
     {
-        id:7,
+        id:17,
         title:"Golmaal: Fun Unlimited",
-        category: "comedy",
-        img: "/images/item21.jpg",
-        rating:" IMDB rating : 7.3/10",
+        genres: "comedy",
+        img: "/images/item21.jpeg",
+        rating:`IMDB rating : 7.3/10`,
         desc:` Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi sit atque cumque natus totam magnam! Assumenda magnam nihil mollitia accusantium velit perferendis labore,`,
         link:`https://www.hotstar.com/in/movies/golmaal-fun-unlimited/1260132192`
     
     },
     {
-        id:7,
+        id:18,
         title:"Pushpa:The Rise",
-        category: "action",
+        genres: "action",
         img: "/images/item17.jpg",
-        rating:" IMDB rating : 7.3/10",
+        rating:`IMDB rating : 7.3/10`,
         desc:`Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi sit atque cumque natus totam magnam! Assumenda magnam nihil mollitia accusantium velit perferendis labore.`,
         link:`https://www.justwatch.com/us/movie/pushpa-the-rise-part-1`
     
     },
 
     {
-        id:7,
+        id:19,
         title:"anweshippin kandethum ",
-        category: "Mystery",
-        img: "/images/item27.jpg",
-        rating:" IMDB rating : 7.3/10",
+        genres: "Mystery",
+        img: "/images/item28.jpg",
+        rating:`IMDB rating : 7.3/10`,
         desc:` Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi sit atque cumque natus totam magnam! Assumenda magnam nihil mollitia accusantium velit perferendis labore.`,
         link:`https://www3.stage.netflix.com/in/title/81602976`
     
     },
 
     {
-        id:7,
+        id:20,
         title:"Mad Max: Fury Road ",
-        category: "action",
+        genres: "action",
         img: "/images/item19.jpg",
-        rating:" IMDB rating : 7.3/10",
+        rating:`IMDB rating : 7.3/10`,
         desc:` Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi sit atque cumque natus totam magnam! Assumenda magnam nihil mollitia accusantium velit perferendis labore.`,
         link:`https://www.jiocinema.com/movies/mad-max-fury-road/3756711`
     
     },
     {
-        id:7,
+        id:21,
         title:"Jamaba Lakidi Pamba",
-        category: "comedy",
+        genres: "comedy",
         img: "/images/item20.jpg",
-        rating:" IMDB rating : 7.3/10",
+        rating:` IMDB rating : 7.3/10`,
         desc:` Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi sit atque cumque natus totam magnam! Assumenda magnam nihil mollitia accusantium velit perferendis labore,`,
         link:`https://en.kinorium.com/204892/`
     
     },
 
     {
-        id:7,
+        id:22,
         title:"Harry Potter and the Sorcerer's Stone (2001)",
-        category: "Fantasy",
+        genres: "Fantasy",
         img: "/images/item12.jpg",
-        rating: " IMDB rating : 7.3/10",
+        rating: ` IMDB rating : 7.3/10`,
         desc:`In this story follows an orpahaned boy name is Harry Potter,who discovers on his elevent birthday that he is a wizard and how he learns about his magical heritage and past.`,
         link:`https://www.justwatch.com/us/movie/harry-potter-and-the-philosophers-stone`
     
     },
     
     {
-        id:7,
+        id:23,
         title:"Bhool Bhulaiyaa",
-        category: "comedy",
+        genres: "comedy",
         img: "/images/item23.jpg",
-        rating:" IMDB rating : 7.3/10",
+        rating:`IMDB rating : 7.3/10`,
         desc:` Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi sit atque cumque natus totam magnam! Assumenda magnam nihil mollitia accusantium velit perferendis labore.`,
         link:`https://www.justwatch.com/in/movie/bhool-bhulaiyaa`
     
@@ -251,33 +259,33 @@ const movie = [
     },
 
     {
-        id:5,
+        id:24,
         title: "The Exorcist",
-        category: "Horror",
+        genres: "Horror",
         img: "/images/item5.jpg",
-        rating:" IMDB rating : 7.3/10",
+        rating:`IMDB rating : 7.3/10`,
         desc:`When a 12-year-old girl is possessed by a mysterious entity, her mother seeks the help of two priests to save her.`,
         link:`https://www.moviefone.com/movie/the-exorcist/4801/main/`
   
     },
 
     {
-        id:7,
+        id:25,
         title:"Superbad",
-        category: "comedy",
+        genres: "comedy",
         img: "/images/item24.jpg",
-        rating:15.66,
+        rating:`IMDB rating : 7.3/10`,
         desc:` Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi sit atque cumque natus totam magnam! Assumenda magnam nihil mollitia accusantium velit perferendis labore.`,
         link:`https://tubitv.com/movies/710383/superbad`
     
     
     },
     {
-        id:7,
+        id:26,
         title:"Sherlock Holmes",
-        category: "Mystery",
+        genres: "Mystery",
         img: "/images/item26.jpg",
-        rating:" IMDB rating : 7.3/10",
+        rating:`IMDB rating : 7.3/10`,
         desc:` Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi sit atque cumque natus totam magnam! Assumenda magnam nihil mollitia accusantium velit perferendis labore.`,
         link:`https://www1.123moviesme.online/sherlock-holmes-2009/`
     
@@ -285,25 +293,56 @@ const movie = [
     },
 
     {
-        id:7,
+        id:27,
         title:"Sye",
-        category: "action",
+        genres: "action",
         img: "/images/item18.jpg",
-        rating:" IMDB rating : 7.3/10",
+        rating:`IMDB rating : 7.3/10`,
         desc:`Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi sit atque cumque natus totam magnam! Assumenda magnam nihil mollitia accusantium velit perferendis labore.`,
         link:`https://www.youtube.com/watch?v=1_T9JgUmuPc`
     
     },
     
     {
-        id:7,
+        id:28,
         title:"The Hangover",
-        category: "comedy",
+        genres: "comedy",
         img: "/images/item25.jpg",
-        rating:" IMDB rating : 7.3/10",
+        rating:`IMDB rating : 7.3/10`,
         desc:` Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi sit atque cumque natus totam magnam! Assumenda magnam nihil mollitia accusantium velit perferendis labore.`,
         link:`https://flixup.to/watch-movie/watch-the-hangover-full-19008.5298652`
     
     
     },
 ]
+
+const sectionCenter = document.querySelector(".section-center");
+const btnContainer = document.querySelector(".btn-container");
+
+window.addEventListener("DOMContentLoaded", () => {
+    displayMovieInfo(movie);
+    displayMovieButtons();
+   
+ });
+
+ function displayMovieInfo (movieItems){
+    let displayMovie = movieItems.map((info) => {
+
+        return `<article class="menu-item">
+                     
+        <div class="item-info">
+           <header>
+               <h4>${info.title}</h4>
+           </header>
+           <img src=${info.img} class="photo" alt=${info.title} />
+           <span class="rating">${info.rating}</span>
+           <p class="movie-text">${info.desc}</p>
+            
+      </div>
+ </article> `;
+
+    });
+
+    displayMovie = displayMovie.join("");
+    sectionCenter.innerHTML = displayMovie;
+}
